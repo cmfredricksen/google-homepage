a82c67e9ba00a50d9b3a083bddb19f6339bd1d69
Christine <christine@christinefredricksen.com>
2020-08-27 15:45:51 -0500

    trying different solutions to my pigLation translator.

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,61 @@ function whatIsInAName(collection, source) {
 
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
 );
+
+function translatePigLatin(str) {
+	let consonantRegex = /^[^aeiou]+/;
+	let myConsonants = str.match(consonantRegex);
+	return myConsonants !== null
+		? str
+			.replace(consonantRegex, '')
+			.concat('ay')
+		: str.concat('way');
+}
+console.log(translatePigLatin("elementary"));
+
+function translatePigLatin2(str) {
+	var pigLatin = '';
+	var regex = /[aeiou]/gi;
+
+	if (str[0].match(regex)) {
+		pigLatin = str + 'way';
+	} else if (str.match(regex) === null) {
+		pigLatin = str + 'ay';
+	} else {
+		var vowelIndice = str.indexOf(str.match(regex)[0]);
+		pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
+	}
+	return pigLatin;
+}
+
+console.log(translatePigLatin2('fresh')); //returns undefined
+
+
+function translatePigLatin3(str) {
+	if (str.match(/^[aeiou]/)) return str + 'way';
+	
+	const consonantCluster = str.match(/^[^aeiou]+/)[0];
+	return str.substr(consonantCluster.length) + consonantCluster + 'ay';
+}
+
+console.log(translatePigLatin3('holiday'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
